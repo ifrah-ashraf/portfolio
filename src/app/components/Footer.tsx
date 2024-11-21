40435ae2e6bbd46c4c16ccc6dad6b5cc6134a58c
ifrah-ashraf <ifrahashraf48@gmail.com>
2024-11-22 00:38:19 +0530

    footer updated

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer: React.FC = () => {
     return (
-      <footer className=" text-gray-400 text-center py-2">
-        <p className="text-md space-x-4">
+      <footer className=" text-gray-400 text-center py-2 mb-0">
+        <p className="text-md ">
           © {new Date().getFullYear()} Ifrah Ashraf. All rights reserved.
         </p>
       </footer>
