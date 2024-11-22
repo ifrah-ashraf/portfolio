06ea5bf20352c3dfc5d15bfbfc2f091dea03f4ec
ifrah-ashraf <ifrahashraf48@gmail.com>
2024-11-22 12:36:24 +0530

    updated navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="sticky top-0 z-50 bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm shadow-lg ">
             <div className="max-w-4xl mx-auto px-4">
-                <div className="flex items-center justify-evenly h-16">
+                <div className="flex items-center justify-evenly h-12 md:h-16">
                     <div className="flex space-x-4 text-white">
                         <a href="#project" className="hover:text-gray-300 transition text-lg font-semibold">Projects</a>
                         <a href="https://github.com/ifrah-ashraf/" target="_blank" className="hover:text-gray-300 transition"><FaGithub className="text-2xl" /></a>
